d08a9fa7453e3f0401a08d5f7ce48a9b20c0c50a
purav juneja <puravjuneja@gmail.com>
2021-10-22 22:55:29 +0530

    dev

--- indexscript.js
+++ indexscript.js
@@ -2,25 +2,30 @@
     //Do work
 }); */
 var totalbill;
-var billratio;
+var billratio = 0;
 var noOfPeople;
-var tipPerPerson = "$0.00";
+var tipPerPerson;
+var billPerPerson;
 
 function calcBill(){
-    if(noOfPeople==0 || ){
-        document.getElementById("tip-result").innerText = '₹0.00';    
-    }
+    if(noOfPeople==0 || document.querySelector("#input-box-2 input").value==""){
+        document.getElementById("tip-result").innerText = '₹0.00';
+        document.getElementById("contri-result").innerHTML = "₹0.00";    
+    }else{
     tipPerPerson = ((totalbill * (billratio/100))/noOfPeople).toFixed(2);
-    document.getElementById("tip-result").innerText = tipPerPerson;
+    billPerPerson = (totalbill/noOfPeople).toFixed(2);
+    document.getElementById("tip-result").innerText = '₹'+ tipPerPerson;
+    document.getElementById("contri-result").innerText = '₹'+ billPerPerson;
+    }
 }
 
 
 // dealing with total bill
-document.getElementById("bill-input").onchange = function(event){
+document.getElementById("bill-input").addEventListener('input',function(event){
     totalbill = event.target.value;
     totalbill = Number(totalbill);
     calcBill();
-}
+});
 
 document.getElementById("input-box").onclick = function(){
     document.getElementById("input-box").style.border = "solid 2px hsl(172, 67%, 45%)";
@@ -40,19 +45,21 @@ for(let a=0;a<billRatioButton.length;a++){
             billRatioButton[a].style.backgroundColor = "hsl(172, 67%, 45%)";
             billRatioButton[a].style.color = "hsl(183, 100%, 15%)";
             billratio = Number(billRatioButton[a].innerHTML.replace("%",""));
+            calcBill();
         }
 }
 
 document.querySelector("#select-tip-section input").onclick = function(){
     for(var b=0;b<billRatioButton.length;b++){
         billRatioButton[b].style.backgroundColor = "hsl(183, 100%, 15%)";
-        billRatioButton[b].style.color = "hsl(0, 0%, 100%)";               
+        billRatioButton[b].style.color = "hsl(0, 0%, 100%)";              
     }
     document.querySelector("#select-tip-section input").style.border = "solid 2px hsl(172, 67%, 45%)";
 }
-document.querySelector("#select-tip-section input").onchange = function(b){
-    billratio = b.target.value;
-}
+document.querySelector("#select-tip-section input").addEventListener('input',function(event){
+    billratio = event.target.value;
+    calcBill();
+});
 
 // dealing with total people
 
@@ -62,7 +69,7 @@ document.querySelector("#input-box-2 input").onclick = function(){
     }
     document.querySelector("#input-box-2").style.border = "solid 2px hsl(172, 67%, 45%)";
 }
-document.querySelector("#input-box-2 input").onchange = function(event){
+document.querySelector("#input-box-2 input").addEventListener("input",function(event){
     if(event.target.value == 0){
         document.getElementById("warning").innerText = "Can't be Zero";
         document.getElementById("warning").style.color = "red";
@@ -75,7 +82,12 @@ document.querySelector("#input-box-2 input").onchange = function(event){
     noOfPeople = Number(noOfPeople);
     calcBill();
     }
+});
+
+
+document.getElementById("button").onclick() = function(){
+    for(var b=0;b<billRatioButton.length;b++){
+        billRatioButton[b].style.backgroundColor = "hsl(183, 100%, 15%)";
+        billRatioButton[b].style.color = "hsl(0, 0%, 100%)";              
+    }
 }
-
-
-// dealing with result
